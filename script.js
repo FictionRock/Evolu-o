a583ddce328e3aabe23e995f94ccb4690221a5d4
FictionRock <147036452+FictionRock@users.noreply.github.com>
2025-04-16 10:41:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ document.addEventListener('DOMContentLoaded', function() {
     // Alerta quando clicar no título principal
     const titulo = document.querySelector('h1');
     titulo.addEventListener('click', function() {
-        alert('Clica aqui não caraio &#129324;!');
+        alert('Clica aqui não caraio!');
     });
 });
